e55b63ff5362cd04f5b03ae14571904c4c8f0780
callunity <heather.e.armstrong@gmail.com>
2014-08-19 12:48:16 -0400

    Added Contacts and New Contact pages (erb)

--- crm.rb
+++ crm.rb
@@ -7,12 +7,19 @@ require 'sinatra'
 
 # routes
 get '/' do
-  @crm_app_name = "My CRM"
+  @crm_app_name = "Today's CRM"
   erb :index
 end
 
 get '/contacts' do
   erb :contacts
+end
+
+post '/contacts' do ## implement meeeeeeeee
+  puts params
+  contact = Contact.new(params['first_name'], params['last_name'], params['email'], params['notes'])
+  @@rolodex.add_contact(contact)
+  redirect to('/contacts')
 end
 
 get '/contacts/new' do
